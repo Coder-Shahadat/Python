9bee4f4cda85b28ea1696873075096b51d7174fa
MD Shahadat Hossain Bhuian <103915287+Coder-Shahadat@users.noreply.github.com>
2022-08-27 14:56:01 +0600

    Update cartesian product.py

--- cartesian product.py
+++ cartesian product.py
@@ -1,8 +1,6 @@
 from itertools import product
 
-with open("p1.txt") as g:
-    s = list(map(int, g.readlines()))
-print("S = " + str(s))
+s = list(map(int, input().split()))
 
 res = [(i, j) for i, j in product(s, repeat=2) if i % j == 0]
 res2 = [(i, j) for i, j in product(s, repeat=2) if i <= j]
